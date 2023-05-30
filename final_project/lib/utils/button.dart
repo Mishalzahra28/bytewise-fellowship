d056be4ea7b59deef9142f440370a528c0277d5a
Mishal Zahra <mishalzahra281@gmail.com>
2023-05-30 18:49:17 +0500

    splash Screen

---
+++ final_project/lib/utils/button.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class CustomBtn extends StatelessWidget {
+  late final String text;
+  VoidCallback onBtnPressed;
+  CustomBtn({
+    super.key,
+    required this.text,
+    required this.onBtnPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Material(
+        elevation: 5,
+        color: Colors.blue,
+        borderRadius: BorderRadius.circular(25),
+        child: MaterialButton(
+          onPressed: onBtnPressed,
+          minWidth: 300,
+          height: 52,
+          child: Text(
+            text,
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ final_project/lib/views/splashScreen/splash.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import "../../utils/button.dart";
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+            ),
+            Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Image.asset(
+                    "lib/assets/logo.png",
+                    height: 200,
+                    width: 200,
+                  ),
+                ),
+                const Text("See whats happening around the world right now.",
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold))
+              ],
+            ),
+            const SizedBox(
+              height: 150,
+            ),
+            Column(
+              children: [
+                CustomBtn(text: "Login", onBtnPressed: () {}),
+                CustomBtn(text: "Sign Up", onBtnPressed: () {})
+              ],
+            )
+          ],
+        ),
+      ),
+    ));
+  }
+}
