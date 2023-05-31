2654591b64433215b4c81c6a2e95f06db2703a3c
Mishal Zahra <mishalzahra281@gmail.com>
2023-05-31 22:20:50 +0500

    register user

--- final_project/lib/main.dart
+++ final_project/lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import "./views/splashScreen/splash.dart";
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
--- final_project/lib/views/register/register.dart
+++ final_project/lib/views/register/register.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import "../../utils/customField.dart";
 import "../../utils/button.dart";
+import "../../services/authServices.dart";
 
 class Register extends StatefulWidget {
   @override
@@ -8,9 +9,21 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
-  final _uName = TextEditingController();
-  final _email = TextEditingController();
-  final _password = TextEditingController();
+  late String name;
+  final _uNameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  void register() async {
+    bool isValid = await AuthService.signUp(
+        _uNameController.text, _emailController.text, _passwordController.text);
+    if (isValid) {
+      Navigator.pop(context);
+    } else {
+      print('Something went wrong');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,19 +54,19 @@ class _RegisterState extends State<Register> {
             ),
             CustomField(
               text: "Username",
-              textController: _uName,
+              textController: _uNameController,
             ),
             CustomField(
               text: "Email",
-              textController: _email,
+              textController: _emailController,
             ),
             CustomField(
               text: "Password",
-              textController: _password,
+              textController: _passwordController,
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: [CustomBtn(text: "Signup", onBtnPressed: () {})],
+              children: [CustomBtn(text: "Signup", onBtnPressed: register)],
             ),
           ],
         ),
